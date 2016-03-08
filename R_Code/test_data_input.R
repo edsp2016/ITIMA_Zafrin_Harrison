8f7f6ca42b6d3a9a592e27ba0b4cf9af4e448eb0
Yoav Bergner <ybergner@gmail.com>
2016-03-08 13:06:01 -0500

    cleaning up example

--- R_Code/test_data_input.R
+++ R_Code/test_data_input.R
@@ -1,21 +1,27 @@
 # Load 
-path <- '/Users/harrison/Desktop/test.txt'
-path2 <- '/Users/harrison/Desktop/Thesis_Test/Ariana Grande - One Last Time/Test/Ld Voc Stem_converted_normalized.csv'
 
-data <- read.csv(path2, header = FALSE)
+# path <- '/Users/harrison/Desktop/test.txt'
+path <- '../Data/Ariana Grande - One Last Time/Test/Ld Voc Stem_converted_normalized.csv'
+# something is wrong with this file
 
-test_data <- read.csv(path, header = FALSE)
+# data <- read.csv(path2, header = FALSE)
+# test_data <- read.csv(path, header = FALSE)
 
-data_3 <- readLines(path2, n=3)
+# test with small number of lines
+numLines <- 3
+
+rawdata <- readLines(path, n=numLines) # remove n=numLines to load all
+numLines <- length(rawdata)
 allvars <- c()
-for (i in 1:3) {
-  line2 <- data_3[i]
-  line2split <- strsplit(line2,",")[[1]]
-  varname <- line2split[1]
-  # trim leading whitespace and/or check if legal name
-  values <- as.numeric(line2split[2:length(line2split)])
+
+for (i in 1:numLines) {
+  currentline <- rawdata[i]
+  currentline_split <- strsplit(currentline,",")[[1]]
+  varname <- currentline_split[1]
+  # TODO: trim leading whitespace and/or check if legal name
+  values <- as.numeric(currentline_split[2:length(currentline_split)])
+  # TODO: verify that this works
   assign(varname,values)
   allvars <- c(allvars,varname)
 }  
-# convert the results of readLines into two variable vectors named "row1" and "row2" or whatever the first element of the row actually is.
 
